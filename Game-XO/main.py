d8b2edbdcc45ead897e906ce1d8a185b13f5f915
Roman <romanvytvytskyy@yahoo.com>
2022-12-08 18:48:31 +0200

    v0.2

--- Game-XO/main.py
+++ Game-XO/main.py
@@ -1,29 +1,82 @@
 from turtle import *
 from random import shuffle, randint
 from time import sleep
-from settings import field, cros, circle
+from settings import field, cros, circle, dance, check_winner
 
 
 field()
 
+
+tile_coor = [(-100, 100), (0, 100), (100, 100),
+             (-100, 0), (0, 0), (100, 0), (-100, -100), (0, -100), (100, -100)]
 tiles = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-print("Cross turn")
-turn = int(input("Enter tile number"))
-if tiles[turn-1] == 0:
-    cros(-100, 100)
-    tiles[turn-1] = 1
-    print(tiles)
+
+game = True
+turn = 1  # 1 - X-turn, 2- 0-turn
 
 
-print("Circle turn")
-turn = int(input("Enter tile number"))
-if tiles[turn-1] == 0:
-    circle(100, 100)
-    tiles[turn-1] = 2
-    print(tiles)
+while game:  # ==  while game == True:
+    if turn == 1:
+        print("Cross turn")
+        turn_tile = int(input("Enter tile number (1-9)"))
+        if 0 < turn_tile < 10 and tiles[turn_tile-1] == 0:
+            if turn_tile == 1:
+                cros(-100, 100)
+            elif turn_tile == 2:
+                cros(0, 100)
+            elif turn_tile == 3:
+                cros(100, 100)
+            elif turn_tile == 4:
+                cros(-100, 0)
+            elif turn_tile == 5:
+                cros(0, 0)
+            elif turn_tile == 6:
+                cros(100, 0)
+            elif turn_tile == 7:
+                cros(-100, -100)
+            elif turn_tile == 8:
+                cros(0, -100)
+            elif turn_tile == 9:
+                cros(100, -100)
+            else:
+                print("Enter tile number from 1 to 9")
+            tiles[turn_tile-1] = 1
+            turn = 2
+            print(tiles)
+        else:
+            print("Wrong tile. Or tile is ocupated")
+        check_winner(tiles, tile_coor, game)
+    if turn == 2:
+        print("Circle turn")
+        turn_tile = int(input("Enter tile number (1-9)"))
+        if 0 < turn_tile < 10 and tiles[turn_tile-1] == 0:
+            if turn_tile == 1:
+                circle(-100, 100)
+            elif turn_tile == 2:
+                circle(0, 100)
+            elif turn_tile == 3:
+                circle(100, 100)
+            elif turn_tile == 4:
+                circle(-100, 0)
+            elif turn_tile == 5:
+                circle(0, 0)
+            elif turn_tile == 6:
+                circle(100, 0)
+            elif turn_tile == 7:
+                circle(-100, -100)
+            elif turn_tile == 8:
+                circle(0, -100)
+            elif turn_tile == 9:
+                circle(100, -100)
+            else:
+                print("Enter tile number from 1 to 9")
+            tiles[turn_tile-1] = 2
+            print(tiles)
+            turn = 1
+        else:
+            print("Wrong tile. Or tile is ocupated")
+        check_winner(tiles, tile_coor, game)
 
 
-#cros(-100, 100)
-#circle(100, 100)
 exitonclick()
--- Game-XO/settings.py
+++ Game-XO/settings.py
@@ -7,6 +7,9 @@ blue = 'blue'
 t1 = Turtle()
 t2 = Turtle()
 t3 = Turtle()
+t1.hideturtle()
+t2.hideturtle()
+t3.hideturtle()
 
 t1.color(black)
 t1.width(5)
@@ -26,6 +29,7 @@ t3.shape('circle')
 def field(x=-150, y=150):
     t1.penup()
     t1.goto(x, y)
+    t1.showturtle()
     t1.pendown()
     i = 0
     while i < 4:
@@ -55,16 +59,84 @@ def field(x=-150, y=150):
 def cros(x=0, y=0):
     t2.penup()
     t2.goto(x-50, y+50)
+    t2.showturtle()
     t2.pendown()
     t2.goto(x+50, y-50)
     t2.penup()
     t2.goto(x+50, y+50)
     t2.pendown()
     t2.goto(x-50, y-50)
+    t2.hideturtle()
 
 
 def circle(x=0, y=0):
     t3.penup()
     t3.goto(x, y-50)
+    t3.showturtle()
     t3.pendown()
     t3.circle(50)
+    t3.hideturtle()
+
+
+def check_winner(tiles, tile_coor, game):
+    winner = 0
+    if tiles[0] == tiles[1] == tiles[2]:
+        winner = tiles[0]
+        if winner > 0:
+            cross(tile_coor[0], tile_coor[2])
+    elif tiles[3] == tiles[4] == tiles[5]:
+        winner = tiles[3]
+        if winner > 0:
+            cross(tile_coor[3], tile_coor[5])
+    elif tiles[6] == tiles[7] == tiles[8]:
+        winner = tiles[6]
+        if winner > 0:
+            cross(tile_coor[6], tile_coor[8])
+    elif tiles[0] == tiles[3] == tiles[6]:
+        winner = tiles[0]
+        if winner > 0:
+            cross(tile_coor[0], tile_coor[6])
+    elif tiles[1] == tiles[4] == tiles[7]:
+        winner = tiles[1]
+        if winner > 0:
+            cross(tile_coor[1], tile_coor[7])
+    elif tiles[2] == tiles[5] == tiles[8]:
+        winner = tiles[2]
+        if winner > 0:
+            cross(tile_coor[2], tile_coor[8])
+    elif tiles[0] == tiles[4] == tiles[8]:
+        winner = tiles[0]
+        if winner > 0:
+            cross(tile_coor[0], tile_coor[8])
+    elif tiles[2] == tiles[4] == tiles[6]:
+        winner = tiles[2]
+        if winner > 0:
+            cross(tile_coor[2], tile_coor[6])
+    if winner == 1:
+        print("Cross win")
+        dance(t2)
+    if winner == 2:
+        print("Zero win")
+        dance(t3)
+
+    game = False
+
+
+def cross(start, finish):
+    t1.width(7)
+    t1.penup()
+    t1.goto(*start)
+    t1.pendown()
+    t1.goto(*finish)
+    t1.penup()
+
+
+def dance(t):
+    t.speed(50)
+    for i in range(10):
+        t.penup()
+        t.goto(-250, 250)
+        t.pendown()
+        for j in range(25):
+            t.forward(10)
+            t.left(10)
